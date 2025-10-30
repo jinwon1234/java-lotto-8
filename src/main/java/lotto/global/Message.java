e0448f1cbcda09714f413b5c6404c51308540489
jinwon1234 <ljw0626@naver.com>
2025-10-30 15:22:45 +0900

    feat: ErrorMessage, Message 클래스 생성

---
+++ src/main/java/lotto/global/Message.java
@@ -0,0 +1,10 @@
+package lotto.global;
+
+import static lotto.global.ErrorMessage.*;
+
+public final class Message {
+
+    private Message() {
+        throw new AssertionError(INVALID_CONSTRUCTOR);
+    }
+}
